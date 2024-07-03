78e122822f1f4b3d68c9d92ae8f4be26527ee995
ankit <ankit@bellurbis.com>
2024-07-03 17:07:59 +0530

    done

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
     addressHostName: 'https://nominatim.openstreetmap.org',
-    apiHostName:'http://localhost:8000'
+    apiHostName:'https://ev-backend-chi.vercel.app/'
 };
